d17f8a376e05e9e8a0162416b5a5e0742f283fb7
Lucas Ayres <35879083+MagnusTheRed@users.noreply.github.com>
2024-12-03 08:41:17 +0000

    Update PotionUse.cs

--- PotionUse.cs
+++ PotionUse.cs
@@ -1,15 +1,3 @@
-/*
-Lucas Ayres Student ID 2321346 
-All code was created using the following sources:
- Pandemonium(4/12/2020) Unity 2D Platformer for Complete Beginners Playlist Accessed: 20/10/2024
- Game Maker's Toolkit(2/12/2022) The Unity Tutorial For Complete Beginners Accessed: 20/10/2024
- Brackey's(22/1/2017) How to make a Video Game - Getting Started (Unity) Accessed: 20/10/2024
- Stack Overflow Community. (2015). Spawning player at certain point in Unity [Online]. Stack Overflow. 
- Available from: https://stackoverflow.com/questions/31565355/spawning-player-at-certain-point-in-unity?rq=3 [Accessed 27 Oct 2024]
- Unity Documentation was also used but I didn't get anything the series did not cover. 
- ChatGTP was also used to grade Code for maintability and usability - no content generated only gave suggestions
-*/
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,6 +7,7 @@ public class PotionUse : MonoBehaviour
 {
     public string potionName = "Potion"; // Name of item
     public int potionScoreValue = 100; // Effect of potion (add score in this case)
+    public ParticleSystem potionEffect; // Reference to the particle system
 
     private void Update()
     {
@@ -31,7 +20,7 @@ public class PotionUse : MonoBehaviour
 
     private void UsePotion()
     {
-        //Check InventoryManager to find and use potions
+        // Check InventoryManager to find and use potions
         InventoryManager inventory = FindObjectOfType<InventoryManager>();
         if (inventory != null)
         {
@@ -54,12 +43,34 @@ public class PotionUse : MonoBehaviour
 
     private void ApplyPotionEffect()
     {
-        // Add score ( or potentially other effects) for using the potion
+        // Add score (or potentially other effects) for using the potion
         ScoreManager scoreManager = FindObjectOfType<ScoreManager>();
         if (scoreManager != null)
         {
             scoreManager.AddScore(potionScoreValue);
             Debug.Log($"{potionName} used! {potionScoreValue} points added.");
+        }
+
+        // Play the potion particle effect
+        if (potionEffect != null)
+        {
+            potionEffect.Play();
+
+            // Optionally stop the particle effect after its duration
+            StartCoroutine(StopPotionEffectAfterDuration(potionEffect.main.duration));
+        }
+        else
+        {
+            Debug.LogWarning("Potion particle effect is not assigned!");
+        }
+    }
+
+    private IEnumerator StopPotionEffectAfterDuration(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        if (potionEffect != null)
+        {
+            potionEffect.Stop();
         }
     }
 }
